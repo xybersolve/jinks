ef0b7e270a0932d4d2448018d8162e875b01ec96
xybersolve <xybersolve@gmail.com>
2018-04-06 13:35:11 -0700

    change default Jenkins home to ~/.jenkins

--- jinks.conf.sh
+++ jinks.conf.sh
@@ -3,5 +3,5 @@ declare -r ARCHIVE_DIR=~/jenkins/archives
 declare -r JENKINS_WAR_BASE_DIR=~/jenkins/war-files
 
 # user assignable variables, default settings
-declare JENKINS_HOME_DIR=~/jenkins_home
+declare JENKINS_HOME_DIR=~/.jenkins
 declare JENKINS_VER='2.111'
