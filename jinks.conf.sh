4e022bfc10ae465dd5a231289a5580f814cd089a
xybersolve <xybersolve@gmail.com>
2018-05-25 17:10:17 -0700

    initial commit

--- jinks.conf.sh
+++ jinks.conf.sh
@@ -3,7 +3,7 @@ declare -r ARCHIVE_DIR=~/jenkins/archives
 declare -r JENKINS_WAR_BASE_DIR=~/jenkins/war-files
 
 # user assignable variables
-declare JENKINS_HOME_DIR=~/jenkins_home
+declare JENKINS_HOME_DIR=~/.jenkins
 declare JENKINS_VER='2.107'
 
 # docker settings
