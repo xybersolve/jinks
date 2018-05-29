796a4955f207080b399dbe8c0e648f63f50dc7d3
xybersolve <xybersolve@gmail.com>
2018-05-28 19:16:42 -0700

    refactor to docker only implementation - WAR version now in xs-janks

--- jinks.conf.sh
+++ jinks.conf.sh
@@ -1,12 +1,13 @@
 # local jinks settings
-declare -r ARCHIVE_DIR=~/jenkins/archives
-declare -r JENKINS_WAR_BASE_DIR=~/jenkins/war-files
+declare -r JINKS_DATA_DIR=~/jenkins
+declare -r ARCHIVE_DIR=${JINKS_DATA_DIR}/archives
 
 # user assignable variables
+# home directory - i.e, master workspace found within
 declare JENKINS_HOME_DIR=~/.jenkins
-declare JENKINS_VER='2.107'
 
 # docker settings
+declare ORG=xybersolve
 # oldschool jenkins
 #declare JENKINS_IMAGE=jenkinsci/jenkins
 #declare JENKINS_CONTAINER=jenkins-oldschool
@@ -16,5 +17,10 @@ declare JENKINS_VER='2.107'
 #declare JENKINS_CONTAINER=jenkins-blueocean
 
 # xybersolve jenkins blueocean
+declare JENKINS_VOLUME=jenkins_home
 declare JENKINS_IMAGE=xybersolve/xs-jenkins-blue:latest
 declare JENKINS_CONTAINER=xs-jenkins-blue
+
+# volume
+declare JENKINS_HOME_CONTAINER=jenkins_home
+declare JENKINS_HOME_IMAGE=xs-jenkins-home
